6a7c69ed1f1755b964816fc9bbb93b5e425116c8
liveinmegacity <postbox461@outlook.com>
2019-08-07 15:27:29 +0700

    Refactor

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
   h = Math.max($(window).height() - $('#idHeader').height(), $('#idVacancy').height());
+  h = Math.max($(window).height() - $('#idVacancy').position().top, $('#idVacancy').height());
   $('#idVacancy').css('min-height', h);
 })
