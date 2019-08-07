bf19a7ec77752ec1ab2136ea69d91c08b93b33e9
liveinmegacity <postbox461@outlook.com>
2019-08-07 17:32:13 +0700

    Refactor

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  h = Math.max($(window).height() - $('#idHeader').height(), $('#idVacancy').height());
-  h = Math.max($(window).height() - $('#idVacancy').position().top, $('#idVacancy').height());
+  // h = Math.max($(window).height() - $('#idHeader').height(), $('#idVacancy').height());
+  h = Math.max($(window).height() - $('#idVacancy').position().top, $('#idVacancy').position().top);
   $('#idVacancy').css('min-height', h);
 })
