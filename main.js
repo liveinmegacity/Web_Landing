239bd0f8329c4e91d62e73f653d068acabe0293b
liveinmegacity <postbox461@outlook.com>
2019-08-08 14:55:09 +0700

    Add gradient slider

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
 $(document).ready(function () {
-  // h = Math.max($(window).height() - $('#idHeader').height(), $('#idVacancy').height());
-  h = Math.max($(window).height() - $('#idVacancy').position().top, $('#idVacancy').position().top);
-  $('#idVacancy').css('min-height', h);
+
+  var block = $('#idVacancy');
+  var top = block.position().top;
+  var height = Math.max($(window).height() - top, top);
+
+  block.css('min-height', height);
+
+  /************************/
+
+  var range = $('#idRange');
+
+  range.on('input', function () {
+    var w = (parseInt(range.val(), 10) - 1) * 25;
+    range.css('background-size', w.toString() + '%');
+  });
+
 })
