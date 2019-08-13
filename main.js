54b5ad8b44b4eb9e2bb3161b22a7fe609ba8a055
liveinmegacity <postbox461@outlook.com>
2019-08-13 10:16:44 +0700

    Improve skills indicator

--- main.js
+++ main.js
@@ -37,7 +37,33 @@ $(window).on('load', function () {
 
     $(title).html($(element).attr('data-title'));
     $(description).html($(element).attr('data-description'));
-    $(segment).attr("stroke", $(element).attr('data-color'));
+    $(segment).attr('stroke', $(element).attr('data-color'));
+    $(segment).attr('d', describeArc(60, 60, 58, 0, 270));
+
   });
 
 });
+
+function polarToCartesian(centerX, centerY, radius, angleInDegrees) {
+  var angleInRadians = (angleInDegrees - 90) * Math.PI / 180.0;
+
+  return {
+    x: centerX + (radius * Math.cos(angleInRadians)),
+    y: centerY + (radius * Math.sin(angleInRadians))
+  };
+}
+
+function describeArc(x, y, radius, startAngle, endAngle) {
+
+  var start = polarToCartesian(x, y, radius, endAngle);
+  var end = polarToCartesian(x, y, radius, startAngle);
+
+  var largeArcFlag = endAngle - startAngle <= 180 ? '0' : '1';
+
+  var d = [
+    'M', start.x, start.y,
+    'A', radius, radius, 0, largeArcFlag, 0, end.x, end.y
+  ].join(' ');
+
+  return d;
+}
